168b4131b5c61e03b7304422afe81494572e96de
raycode21 <wanjiku.rachel21@gmail.com>
2017-04-01 16:27:56 +0300

    blogger2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,21 @@
 class ArticlesController < ApplicationController
-def index
-  @articles = Article.all
+  def create
+  @article = Article.new(
+  title: params[:article][:title],
+body: params[:article][:body])
+  @article.save
+  redirect_to article_path(@article)
+end
+  def new
+    @article = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-end
+
 end
